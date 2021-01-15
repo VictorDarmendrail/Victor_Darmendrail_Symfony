bc87019bccef358c21b091bc915c26e4dc9f4692
Adrien Chignaguet <achignaguet@iutbayonne.univ-pau.fr>
2021-01-15 13:53:22 +0100

    Création de l'entité entreprise

---
+++ src/Entity/Entreprise.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EntrepriseRepository::class)
+ */
+class Entreprise
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=25, nullable=true)
+     */
+    private $Activité;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Site;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getActivité(): ?string
+    {
+        return $this->Activité;
+    }
+
+    public function setActivité(?string $Activité): self
+    {
+        $this->Activité = $Activité;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(?string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getSite(): ?string
+    {
+        return $this->Site;
+    }
+
+    public function setSite(?string $Site): self
+    {
+        $this->Site = $Site;
+
+        return $this;
+    }
+}
